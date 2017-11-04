8e0fb3fc7c513e049b850cb4da5bd1bb2a8127db
Igor_Shchipanov <Igor_Shchipanov@epam.com>
2017-11-04 23:33:45 +0300

    add new bean

--- ioc/src/test/java/HelloWorldTest.java
+++ ioc/src/test/java/HelloWorldTest.java
@@ -1,11 +1,11 @@
 import model.Country;
-import model.Person;
 import model.UsualPerson;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,7 +14,7 @@ class HelloWorldTest {
     private static final String APPLICATION_CONTEXT_XML_FILE_NAME = "application-context.xml";
 
     private UsualPerson expectedPerson;
-    private AbstractApplicationContext context;
+    private BeanFactory context;
 
     @BeforeEach
     void setUp() throws Exception {
@@ -22,28 +22,18 @@ class HelloWorldTest {
         expectedPerson = getExpectedPerson();
     }
 
-    @AfterEach
-    void tearDown() throws Exception {
-        if (context != null) {
-            context.close();
-        }
-    }
-
     @Test
     void testInitPerson() {
-        UsualPerson person = (UsualPerson) context.getBean("person", Person.class);
-        assertEquals(expectedPerson, person);
-        System.out.println(person);
+        assertEquals(expectedPerson, context.getBean("person"));
     }
 
     private UsualPerson getExpectedPerson() {
         UsualPerson person = new UsualPerson();
         person.setAge(35);
         person.setName("John Smith");
-
         Country country = new Country("RU", "Russia", 1);
         person.setCountry(country);
-
+        person.setContacts(Arrays.asList("Impossible", "79118277200"));
         return person;
     }
 
