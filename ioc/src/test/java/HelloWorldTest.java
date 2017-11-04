00fda10763d71237eeedb4e4c5277e6d7565805a
Igor_Shchipanov <Igor_Shchipanov@epam.com>
2017-11-04 22:48:11 +0300

    some refactor

--- ioc/src/test/java/HelloWorldTest.java
+++ ioc/src/test/java/HelloWorldTest.java
@@ -5,21 +5,20 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HelloWorldTest {
 
-    static final String APPLICATION_CONTEXT_XML_FILE_NAME = "/src/test/resources/application-context.xml";
+    private static final String APPLICATION_CONTEXT_XML_FILE_NAME = "application-context.xml";
 
     private UsualPerson expectedPerson;
     private AbstractApplicationContext context;
 
     @BeforeEach
     void setUp() throws Exception {
-        context = new FileSystemXmlApplicationContext(
-                new String[]{APPLICATION_CONTEXT_XML_FILE_NAME});
+        context = new ClassPathXmlApplicationContext(APPLICATION_CONTEXT_XML_FILE_NAME);
         expectedPerson = getExpectedPerson();
     }
 
